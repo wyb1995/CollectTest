b07b072e86956ae55c8dc8ed8a074799fa9692cf
wyb1995 <wy_b1995@163.com>
2016-05-18 18:10:04 +0800

    x

---
+++ src/collect_all_even.js
@@ -0,0 +1,16 @@
+'use strict';
+
+function collect_all_even(collection) {
+  //在这里写入代码
+    var re = /^\d*[02468]$/;
+    var y = collect;
+    var x = new Array();
+    for(var i in y){
+        if(re.test(y[i])){
+            x.push(y[i]);
+        }
+    }
+    return x;
+}
+
+module.exports = collect_all_even;
